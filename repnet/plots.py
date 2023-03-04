0f81ecfc80d06c19f47d4bb24b21e7cfe8724e36
materight <matteo@cerrion.com>
2023-03-04 22:04:32 +0000

    Add periodicity score plot

--- repnet/plots.py
+++ repnet/plots.py
@@ -1,7 +1,7 @@
 """Utility functions for plotting."""
 import cv2
 import numpy as np
-from typing import List
+from typing import List, Optional
 
 
 
@@ -20,23 +20,36 @@ def plot_heatmap(dist: np.ndarray, log_scale: bool = False) -> np.ndarray:
 
 
 
-def plot_repetitions(frames: List[np.ndarray], counts: List[int]) -> List[np.ndarray]:
+def plot_repetitions(frames: List[np.ndarray], counts: List[float], periodicity: Optional[List[float]]) -> List[np.ndarray]:
     """Generate video with repetition counts and return frames."""
     blue_dark, blue_light = (102, 60, 0), (215, 175, 121)
     h, w, _ = frames[0].shape
     pbar_r = max(int(min(w, h) * 0.1), 20)
     pbar_c = (pbar_r + 5, pbar_r + 5)
     txt_s = pbar_r / 30
-    assert len(frames) == len(counts), 'Number of frames and counts must match.'
-    # Draw progress bar
+    assert len(frames) == len(counts) == len(periodicity), 'Number of frames and counts must match.'
+    periodicity = np.asarray(periodicity)
     out_frames = []
-    for frame, count in zip(frames, counts):
+    for i, (frame, count) in enumerate(zip(frames, counts)):
         frame = frame.copy()
+        # Draw progress bar
         color_bg, color_fg = (blue_dark, blue_light) if int(count) % 2 == 0 else (blue_light, blue_dark)
         frame = cv2.ellipse(frame, pbar_c, (pbar_r, pbar_r), -90, 0, 360, color_bg, -1, cv2.LINE_AA)
         frame = cv2.ellipse(frame, pbar_c, (pbar_r, pbar_r), -90, 0, 360 * (count % 1.0), color_fg, -1, cv2.LINE_AA)
         txt_box, _ = cv2.getTextSize(str(int(count)), cv2.FONT_HERSHEY_SIMPLEX, txt_s, 2)
         txt_c = (pbar_c[0] - txt_box[0] // 2, pbar_c[1] + txt_box[1] // 2)
         frame = cv2.putText(frame, str(int(count)), txt_c, cv2.FONT_HERSHEY_SIMPLEX, txt_s, (255, 255, 255), 2, cv2.LINE_AA)
+        # Draw periodicity plot on the right if available
+        if periodicity is not None:
+            padx, pady, window_size = 5, 10, 64
+            pcanvas_h, pcanvas_w = frame.shape[0], min(frame.shape[0], frame.shape[1])
+            pcanvas = np.full((pcanvas_h, pcanvas_w, 3), 255, dtype=np.uint8)
+            pcanvas[pady::int((pcanvas_h - pady*2) / 10), :, :] = (235, 235, 235) # Draw horizontal grid
+            y = ((1 - periodicity[:i+1][-window_size:]) * (pcanvas_h - pady*2) + pady).astype(np.int32)
+            x = ((np.arange(len(y)) / window_size) * (pcanvas_w - padx*2)).astype(np.int32)
+            pts = np.stack([x, y], axis=1).reshape((-1, 1, 2))
+            pcanvas = cv2.polylines(pcanvas, [pts], False, blue_dark, 1, cv2.LINE_AA)
+            pcanvas = cv2.circle(pcanvas, (x[-1], y[-1]), 2, (0, 0, 255), -1, cv2.LINE_AA)
+            frame = np.concatenate([frame, pcanvas], axis=1)
         out_frames.append(frame)
     return out_frames
--- run.py
+++ run.py
@@ -13,11 +13,11 @@ from repnet.model import RepNet
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 OUT_VISUALIZATIONS_DIR = os.path.join(PROJECT_ROOT, 'visualizations')
 SAMPLE_VIDEOS_URLS = [
+    'https://www.reddit.com/r/gifs/comments/4qfif6/cheetah_running_at_63_mph_102_kph', # Cheetah
     'https://imgur.com/t/hummingbird/m2e2Nfa', # Hummingbird
     'https://www.youtube.com/watch?v=5g1T-ff07kM', # Excersise
-    'https://www.reddit.com/r/gifs/comments/4qfif6/cheetah_running_at_63_mph_102_kph', # Cheetah
-    'https://www.youtube.com/watch?v=5EYY2J3nb5c', # Cooking
     'https://www.youtube.com/watch?v=-Q3_7T5w4nE', # Excersise
+    'https://www.youtube.com/watch?v=5EYY2J3nb5c', # Cooking
 ]
 
 # Script arguments
@@ -26,7 +26,7 @@ parser.add_argument('--weights', type=str, default=os.path.join(PROJECT_ROOT, 'c
 parser.add_argument('--video', type=str, default=SAMPLE_VIDEOS_URLS[0], help='Video to test the model on, either a YouTube/http/local path (default: %(default)s).')
 parser.add_argument('--strides', nargs='+', type=int, default=[1, 2, 3, 4], help='Temporal strides to try when testing on the sample video (default: %(default)s).')
 parser.add_argument('--device', type=str, default='cuda', help='Device to use for inference (default: %(default)s).')
-
+parser.add_argument('--no-score', action='store_true', help='If specified, do not plot the periodicity score.')
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -47,7 +47,7 @@ if __name__ == '__main__':
         T.Normalize(mean=0.5, std=0.5),
     ])
     cap = cv2.VideoCapture(video_path)
-    fps, width, height = cap.get(cv2.CAP_PROP_FPS), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
     raw_frames, frames = [], []
     while cap.isOpened():
         ret, frame = cap.read()
@@ -99,8 +99,8 @@ if __name__ == '__main__':
     dist = torch.cdist(best_embeddings, best_embeddings, p=2)**2
     tsm_img = plots.plot_heatmap(dist.numpy(), log_scale=True)
     cv2.imwrite(os.path.join(OUT_VISUALIZATIONS_DIR, 'tsm.png'), tsm_img)
-    rep_frames = plots.plot_repetitions(raw_frames[:len(best_period_count)], best_period_count.tolist())
-    video = cv2.VideoWriter(os.path.join(OUT_VISUALIZATIONS_DIR, 'repetitions.mp4'), cv2.VideoWriter_fourcc(*'mp4v'), fps, (width, height))
+    rep_frames = plots.plot_repetitions(raw_frames[:len(best_period_count)], best_period_count.tolist(), best_periodicity_score.tolist() if not args.no_score else None)
+    video = cv2.VideoWriter(os.path.join(OUT_VISUALIZATIONS_DIR, 'repetitions.mp4'), cv2.VideoWriter_fourcc(*'mp4v'), fps, rep_frames[0].shape[:2][::-1])
     for frame in rep_frames:
         video.write(frame)
     video.release()
